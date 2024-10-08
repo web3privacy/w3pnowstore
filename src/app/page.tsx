6b295a816bc99ea100e74c6c2d7d9c15e34dc7b1
Alissa Crane <alissa.crane@coinbase.com>
2024-10-07 21:28:53 -0700

    adjust layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,6 @@
 "use client";
-import { useAccount } from "wagmi";
 import OnchainStore from "src/components/OnchainStore";
-import Navbar from "src/components/Navbar";
 
 export default function Page() {
-  const { address } = useAccount();
-
-  return (
-    <div className="flex h-full max-w-full flex-col px-1 max-h-screen">
-      <Navbar />
-      <OnchainStore />
-    </div>
-  );
+  return <OnchainStore />;
 }
--- src/components/OnchainStore.tsx
+++ src/components/OnchainStore.tsx
@@ -1,3 +1,4 @@
+import Navbar from "./Navbar";
 import OnchainStoreCart from "./OnchainStoreCart";
 import OnchainStoreItems from "./OnchainStoreItems";
 import { OnchainStoreProvider } from "./OnchainStoreProvider";
@@ -6,13 +7,16 @@ import OnchainStoreSummary from "./OnchainStoreSummary";
 export default function OnchainStore() {
   return (
     <OnchainStoreProvider>
-      <main className="max-w-7xl mx-auto flex flex-col grow py-10">
-        <div className="flex flex-col md:flex-row">
-          <OnchainStoreSummary />
-          <OnchainStoreItems />
-        </div>
-        <OnchainStoreCart />
-      </main>
+      <div className="flex h-full max-w-full flex-col px-1 max-h-screen">
+        <Navbar />
+        <main className="max-w-7xl mx-auto flex flex-col grow py-10">
+          <div className="flex flex-col md:flex-row">
+            <OnchainStoreSummary />
+            <OnchainStoreItems />
+          </div>
+          <OnchainStoreCart />
+        </main>
+      </div>
     </OnchainStoreProvider>
   );
 }
