e81731c388c1f68f23f6f52c89b24eaf99738164
Alissa Crane <alissa.crane@coinbase.com>
2024-10-10 09:39:01 -0700

    add create charge function

--- src/components/OnchainStoreCart.tsx
+++ src/components/OnchainStoreCart.tsx
@@ -1,11 +1,13 @@
 import { useCallback, useMemo } from "react";
 import { useOnchainStoreContext } from "./OnchainStoreProvider";
 import { LifecycleStatus, Pay, PayButton } from "@coinbase/onchainkit/pay";
+import useCreateCharge from "src/hooks/useCreateCharge";
 
-const PRODUCT_ID = "";
 
 export default function OnchainStoreCart() {
   const { quantities, products } = useOnchainStoreContext();
+
+  const { createCharge } = useCreateCharge();
 
   const totalSum = useMemo(() => {
     return (
@@ -20,6 +22,24 @@ export default function OnchainStoreCart() {
     console.log("onStatus", status);
   }, []);
 
+  const chargeHandler = useCallback(() => {
+    const description = Object.keys(quantities)
+      .map((productId) => {
+        return `${productId}(${quantities[productId]})`;
+      })
+      .join(",");
+    const chargeDetails = {
+      name: "commerce template charge",
+      description,
+      pricing_type: "fixed_price",
+      local_price: {
+        amount: totalSum.toString(),
+        currency: "USD",
+      },
+    };
+    return createCharge(chargeDetails);
+  }, [quantities, totalSum]);
+
   return (
     <div className="fixed bottom-0 bg-white border-t border-gray-200 w-screen left-1/2 right-1/2 -mx-[50vw]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-0">
@@ -32,10 +52,11 @@ export default function OnchainStoreCart() {
               TOTAL {totalSum.toFixed(2)} USDC
             </h2>
             <div className="w-64">
-              <Pay onStatus={handleStatusChange} productId={PRODUCT_ID}>
+              <Pay onStatus={handleStatusChange} chargeHandler={chargeHandler}>
                 <PayButton
+                  coinbaseBranded
                   text="Pay with Crypto"
-                  className="bg-black text-white rounded-none hover:bg-[#000000cf]"
+                  disabled={!totalSum}
                 />
               </Pay>
             </div>
---
+++ src/hooks/useCreateCharge.ts
@@ -0,0 +1,50 @@
+import { useCallback } from "react";
+import { COINBASE_COMMERCE_API_KEY } from "src/config";
+
+const COMMERCE_API_URL = "https://api.commerce.coinbase.com";
+
+type Price = {
+  amount: string;
+  currency: string;
+};
+export type ChargeDetails = {
+  name?: string;
+  description?: string;
+  pricing_type?: string;
+  local_price?: Price;
+};
+
+const testOptions = {
+  name: "test charge",
+  description: "chargesss",
+  pricing_type: "fixed_price",
+  local_price: {
+    amount: ".02",
+    currency: "USD",
+  },
+};
+
+const useCreateCharge = () => {
+  const createCharge = useCallback(async (chargeDetails: ChargeDetails) => {
+    console.log({ chargeDetails });
+    try {
+      const res = await fetch(`${COMMERCE_API_URL}/charges`, {
+        method: "POST",
+        body: JSON.stringify(chargeDetails),
+        headers: {
+          "Content-Type": "application/json",
+          "X-CC-Api-Key": COINBASE_COMMERCE_API_KEY,
+        },
+      });
+      const { data } = await res.json();
+      return data.id;
+    } catch (error) {
+      console.error("Error creating charge:", error);
+      throw error;
+    }
+  }, []);
+
+  return { createCharge };
+};
+
+export default useCreateCharge;
