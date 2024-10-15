504bd70003992caa8cfd4147aaadcff9bab79364
Alissa Crane <alissa.crane@coinbase.com>
2024-10-15 10:42:56 -0700

    set default charge amount

--- src/links.ts
+++ src/links.ts
@@ -1,3 +1,4 @@
+export const COMMERCE_API_URL = 'https://api.commerce.coinbase.com';
 export const DISCORD_LINK = 'https://discord.gg/wTJ7besU';
 export const FIGMA_LINK =
   'https://www.figma.com/community/file/1370194397345450683/onchainkit';
