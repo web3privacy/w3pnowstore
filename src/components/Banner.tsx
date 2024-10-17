83fac5140fcfda603a81bae665f5acddb8247826
Alissa Crane <alissa.crane@coinbase.com>
2024-10-17 13:42:46 -0700

    adjust style

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,7 @@
 export function Banner() {
   return (
-    <div className="ock-bg-default-reverse ock-text-inverse fixed top-0 left-0 flex h-14 xs:h-11 w-full flex-wrap items-center justify-center">
-      <div className="flex h-full flex-wrap items-center justify-center text-center text-sm ">
+    <div className="ock-bg-default-reverse ock-text-inverse fixed top-0 left-0 flex h-10 xs:h-11 w-full flex-wrap items-center justify-center sm:h-13">
+      <div className="flex h-full flex-wrap items-center justify-center text-center text-xs sm:text-sm">
         This is a demo site. These products are not for sale.
       </div>
     </div>
--- src/components/OnchainStoreItem.tsx
+++ src/components/OnchainStoreItem.tsx
@@ -4,13 +4,21 @@ import QuantityInput from './QuantityInput';
 
 export default function OnchainStoreItem({ id, name, price, image }: Product) {
   return (
-    <div className="store-item mx-auto flex w-full flex-col border-r p-4 sm:mx-0 lg:p-6">
+    <div className="store-item mx-auto flex w-full flex-col p-4 sm:mx-0 lg:p-6">
       <div className="mb-1 flex items-start justify-between">
         <h2 className="font-regular text-xs">{name}</h2>
-        <p className="font-regular text-xs">{price.toFixed(2)} USDC</p>
       </div>
-      <Image src={image} alt="123" width={280} height={280} />
-      <QuantityInput productId={id} />
+      <Image
+        src={image}
+        alt="123"
+        width={280}
+        height={280}
+        className="mx-auto"
+      />
+      <div className="flex items-center justify-between">
+        <p className="font-regular text-xs">{price.toFixed(2)} USDC</p>
+        <QuantityInput productId={id} />
+      </div>
     </div>
   );
 }
