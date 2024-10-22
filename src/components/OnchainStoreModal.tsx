cb0c04b74250dd8e03bca7ce2460dd90be4974a1
Alissa Crane <alissa.crane@coinbase.com>
2024-10-22 14:29:15 -0700

    fix lint

--- src/components/OnchainStoreModal.tsx
+++ src/components/OnchainStoreModal.tsx
@@ -9,7 +9,7 @@ export default function OnchainStoreModal({
 }: OnchainStoreModalReact) {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="relative z-10 flex max-w-lg flex-col gap-2 xs:rounded-[10px] bg-[white] p-6 px-10 h-full xs:h-auto">
+      <div className="relative z-10 flex h-full xs:h-auto max-w-lg flex-col gap-2 xs:rounded-[10px] bg-[white] p-6 px-10">
         <button
           type="button"
           className="absolute top-2 right-4"
